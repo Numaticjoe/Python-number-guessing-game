f22f1c5c2c76c19fd6fa6dcaa6e07f4b7b791fcf
Numaticjoe <83297576+Numaticjoe@users.noreply.github.com>
2022-03-29 21:08:33 +0100

    Add files via upload
    All the code is in this file 
    You need to download the latest 32 bit python to try it. 
    (link:https://www.python.org/downloads/)

---
+++ Number guess.py
@@ -0,0 +1,19 @@
+# Python testing simple app too see if i like it??
+# Written in VS2019 Community 
+# By Numaticjoe 2022 
+
+import random 
+n = random.randint (1, 10)
+guess = int(input("Type a number between 1 and 10!"))
+while n != "guess":
+	print
+	if guess < n:
+		print ("You guessed too low!")
+		guess = int(input("Type a number between 1 and 10:"))
+	elif guess > n:
+		print ("You guessed too high!")
+		guess = int(input("Type a number between 1 and 10:"))
+
+	else:
+		print ("you guessed it!")
+		break
